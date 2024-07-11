a2b894a803edf58228ef2fd4a3bb9fb7b6c5ba9f
Anton Aleksandrov <anton.aleksandrov@icloud.com>
2024-07-11 17:56:30 +0300

    Added app styles, components styles and InputField component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
 const App: React.FC = () => {
+
+  
   return (
     <div className="App">
       <span className="heading">Did-a-Do</span>
+      <InputField />
     </div>
   );
 }
