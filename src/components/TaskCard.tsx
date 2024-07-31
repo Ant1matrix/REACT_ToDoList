71ca30be89a125986a4eb55d9c24e4e2324b5929
Anton Aleksandrov <anton.aleksandrov@icloud.com>
2024-07-31 17:28:50 +0300

    Finished TaskCard input field functionality and styling, core app functionality and styling done

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,8 +1,8 @@
-import React, {useState} from 'react';
-import { Todo } from '../model';
-import {AiFillEdit, AiFillDelete} from 'react-icons/ai';
-import {MdDone} from 'react-icons/md';
-import './styles.css';
+import React, { useEffect, useRef, useState } from "react";
+import { Todo } from "../model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import "./styles.css";
 
 type Props = {
   todo: Todo;
@@ -10,28 +10,46 @@ type Props = {
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 };
 
-
-const TaskCard = ({todo, todos, setTodos}:Props) => {
-
+const TaskCard = ({ todo, todos, setTodos }: Props) => {
   const [edit, setEdit] = useState<boolean>(false);
   const [editText, setEditText] = useState(todo.todo);
 
-  const handleDone = (id:number) => {
+  const handleDone = (id: number) => {
     setTodos(
-      todos.map((todo)=> 
-      todo.id === id?{...todo,isDone:!todo.isDone}:todo
-    )
-  );
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
   };
 
-  const handleDelete = (id:number) => {
+  const handleDelete = (id: number) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editText } : todo))
+    );
+    setEdit(false);
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
   return (
-    <form className="TaskCard__single">
+    <form className="TaskCard__single" onSubmit={(e) => handleEdit(e, todo.id)}>
       {edit ? (
-        <input value={editText} onChange={(e) => setEditText(e.target.value)} className='editText__box'/>
+        <input
+          ref={inputRef}
+          value={editText}
+          onChange={(e) => setEditText(e.target.value)}
+          className="TaskCard__single--text"
+        />
       ) : todo.isDone ? (
         <s className="TaskCard__single--text">{todo.todo}</s>
       ) : (
